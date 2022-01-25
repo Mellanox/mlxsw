Mellanox/mlxsw

commit c1879318e5b644c2f1fb482812aada6bd76e5065

resmon: Add list_tables RPC
The JSON RPC method get_tables serves for querying of the set of tables
that the daemon knows about. The immediate use-case is querying this for
purposes of dumping, which will be supported in the following patches.

Signed-off-by: Petr Machata <petrm@nvidia.com>
Signed-off-by: Ido Schimmel <idosch@nvidia.com>

Debugging/libbpf-tools/resmon/resmon.c
@@ -44,6 +44,9 @@ static int resmon_cmd(int argc, char **argv)
 	} else if (strcmp(*argv, "stats") == 0) {
 		NEXT_ARG_FWD();
 		return resmon_c_stats(argc, argv);
+	} else if (strcmp(*argv, "dump") == 0) {
+		NEXT_ARG_FWD();
+		return resmon_c_dump(argc, argv);
 	}
 
 	fprintf(stderr, "Unknown command \"%s\"\n", *argv);

Debugging/libbpf-tools/resmon/resmon-c.c
@@ -530,3 +530,102 @@ int resmon_c_stats(int argc, char **argv)
 
 	return resmon_c_stats_jrpc();
 }
+
+static int resmon_c_get_tables_jrpc(void)
+{
+	struct resmon_jrpc_table *tables;
+	struct json_object *response;
+	struct json_object *request;
+	struct json_object *result;
+	const int id = 1;
+	size_t num_tables;
+	char *error;
+	int err = 0;
+
+	request = resmon_jrpc_new_request(id, "get_tables");
+	if (request == NULL)
+		return -1;
+
+	response = resmon_c_send_request(request);
+	if (response == NULL) {
+		err = -1;
+		goto put_request;
+	}
+
+	if (!resmon_c_response_extract_result(response, id, json_type_object,
+					      &result)) {
+		err = -1;
+		goto put_response;
+	}
+
+	if (resmon_c_result_show_json(result)) {
+		err = 0;
+		goto put_result;
+	}
+
+	err = resmon_jrpc_dissect_get_tables(result, &tables, &num_tables,
+					     &error);
+	if (err != 0) {
+		fprintf(stderr, "Invalid tables object: %s\n", error);
+		free(error);
+		goto put_result;
+	}
+
+	if (env.verbosity > 0 && num_tables == 0)
+		fprintf(stderr, "no supported tables\n");
+	for (size_t i = 0; i < num_tables; i++) {
+		if (env.verbosity)
+			fprintf(stdout, "%s seqnn %d nrows %d\n",
+				tables[i].name, tables[i].seqnn,
+				tables[i].nrows);
+		else
+			fprintf(stdout, "%s\n", tables[i].name);
+	}
+	free(tables);
+
+put_result:
+	json_object_put(result);
+put_response:
+	json_object_put(response);
+put_request:
+	json_object_put(request);
+	return err;
+}
+
+static void resmon_c_dump_help(void)
+{
+	fprintf(stderr,
+		"Usage: resmon dump list tables\n");
+}
+
+int resmon_c_dump(int argc, char **argv)
+{
+	bool list_tables = false;
+
+	while (argc > 0) {
+		if (strcmp(*argv, "list") == 0) {
+			NEXT_ARG();
+			if (strcmp(*argv, "tables") != 0)
+				goto incomplete_command;
+			if (list_tables)
+				goto incomplete_command;
+			list_tables = true;
+			NEXT_ARG_FWD();
+		} else if (strcmp(*argv, "help") == 0) {
+			resmon_c_dump_help();
+			return 0;
+		} else {
+			fprintf(stderr, "What is \"%s\"?\n", *argv);
+			return -1;
+		}
+		continue;
+
+incomplete_command:
+		fprintf(stderr, "Command line is not complete. Try option \"help\"\n");
+		return -1;
+	}
+
+	if (list_tables)
+		return resmon_c_get_tables_jrpc();
+	return 0;
+}

Debugging/libbpf-tools/resmon/resmon-d.c
@@ -293,6 +293,111 @@ put_obj:
 	resmon_d_respond_memerr(peer, id);
 }
 
+struct resmon_d_table_info {
+	const char *const name;
+	unsigned int (*nrows)(const struct resmon_stat *stat);
+	unsigned int (*seqnn)(const struct resmon_stat *stat);
+};
+
+static int
+resmon_d_get_tables_attach_table(const struct resmon_stat *stat,
+				 struct json_object *array,
+				 const struct resmon_d_table_info *tinfo)
+{
+	struct json_object *table_obj;
+	unsigned int seqnn;
+	unsigned int nrows;
+
+	table_obj = json_object_new_object();
+	if (table_obj == NULL)
+		return -1;
+
+	seqnn = tinfo->seqnn(stat);
+	nrows = tinfo->nrows(stat);
+
+	if (resmon_jrpc_object_add_str(table_obj, "name", tinfo->name) != 0 ||
+	    resmon_jrpc_object_add_int(table_obj, "seqnn", seqnn) != 0 ||
+	    resmon_jrpc_object_add_int(table_obj, "nrows", nrows) != 0 ||
+	    json_object_array_add(array, table_obj) != 0)
+		goto put_table_obj;
+
+	return 0;
+
+put_table_obj:
+	json_object_put(table_obj);
+	return -1;
+}
+
+static struct resmon_d_table_info resmon_d_tables[] = {
+};
+
+static void resmon_d_handle_get_tables(const struct resmon_stat *stat,
+				       struct resmon_sock *peer,
+				       struct json_object *params_obj,
+				       struct json_object *id)
+{
+	struct json_object *result_obj;
+	struct json_object *array;
+	struct json_object *obj;
+	char *error;
+	int rc;
+
+	/* The response is as follows:
+	 *
+	 * {
+	 *     "id": ...,
+	 *     "result": {
+	 *         "tables": [ {"name": "$NAME", "nrows": $NR, "seqnn": $I }, ...]
+	 *     }
+	 * }
+	 */
+
+	rc = resmon_jrpc_dissect_params_empty(params_obj, &error);
+	if (rc != 0) {
+		resmon_d_respond_invalid_params(peer, id, error);
+		free(error);
+		return;
+	}
+
+	obj = resmon_jrpc_new_object(id);
+	if (obj == NULL)
+		return;
+
+	result_obj = json_object_new_object();
+	if (result_obj == NULL)
+		goto put_obj;
+
+	array = json_object_new_array();
+	if (array == NULL)
+		goto put_result_obj;
+
+	for (size_t i = 0; i < ARRAY_SIZE(resmon_d_tables); i++) {
+		struct resmon_d_table_info *tinfo = &resmon_d_tables[i];
+
+		if (resmon_d_get_tables_attach_table(stat, array, tinfo) != 0)
+			goto put_array;
+	}
+
+	rc = json_object_object_add(result_obj, "tables", array);
+	if (rc != 0)
+		goto put_array;
+
+	if (json_object_object_add(obj, "result", result_obj))
+		goto put_result_obj;
+
+	resmon_jrpc_send(peer, obj);
+	json_object_put(obj);
+	return;
+
+put_array:
+	json_object_put(array);
+put_result_obj:
+	json_object_put(result_obj);
+put_obj:
+	json_object_put(obj);
+	resmon_d_respond_memerr(peer, id);
+}
+
 static void resmon_d_handle_method(struct resmon_back *back,
 				   struct resmon_stat *stat,
 				   struct resmon_reg *rreg,
@@ -311,6 +416,9 @@ static void resmon_d_handle_method(struct resmon_back *back,
 	} else if (strcmp(method, "stats") == 0) {
 		resmon_d_handle_stats(back, stat, rsrc_en, peer,
 				      params_obj, id);
+		return;
+	} else if (strcmp(method, "get_tables") == 0) {
+		resmon_d_handle_get_tables(stat, peer, params_obj, id);
 		return;
 	} else if (resmon_back_handle_method(back, stat, rreg, method,
 					     peer, params_obj, id)) {

Debugging/libbpf-tools/resmon/resmon-jrpc.c
@@ -197,6 +197,35 @@ struct resmon_jrpc_policy {
 	bool required;
 };
 
+static int resmon_jrpc_validate_array(struct json_object *obj,
+				      enum json_type elm_type,
+				      char **error)
+{
+	{
+		enum json_type type = json_object_get_type(obj);
+
+		if (type != json_type_array) {
+			resmon_fmterr(error, "Value expected to be an array, but is %s",
+				      json_type_to_name(type));
+			return -1;
+		}
+	}
+
+	for (size_t i = 0, len = json_object_array_length(obj); i < len; i++) {
+		struct json_object *elm = json_object_array_get_idx(obj, i);
+		enum json_type type = json_object_get_type(elm);
+
+		if (type != elm_type) {
+			resmon_fmterr(error, "Array element %zd is expected to be a %s, but is %s",
+				      i, json_type_to_name(elm_type),
+				      json_type_to_name(type));
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
 static int resmon_jrpc_dissect(struct json_object *obj,
 			       struct resmon_jrpc_policy policy[],
 			       bool seen[],
@@ -296,6 +325,25 @@ static int resmon_jrpc_extract_uint64(const char *key, struct json_object *obj,
 	}
 
 	*pvalue64 = (uint64_t) value;
+	return 0;
+}
+
+static int resmon_jrpc_extract_uint32(const char *key, struct json_object *obj,
+				      uint32_t *pvalue, char **error)
+{
+	uint64_t value64;
+	int err;
+
+	err = resmon_jrpc_extract_uint64(key, obj, &value64, error);
+	if (err != 0)
+		return err;
+
+	*pvalue = value64;
+	if (*pvalue != value64) {
+		resmon_fmterr(error, "Invalid %s: value too large", key);
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -562,6 +610,121 @@ int resmon_jrpc_dissect_stats(struct json_object *obj,
 	return resmon_jrpc_dissect_stats_gauges(values[pol_gauges],
 						gauges, num_gauges,
 						error);
+}
+
+static int
+resmon_jrpc_dissect_tables_table(struct json_object *table_obj,
+				 struct resmon_jrpc_table *ptable,
+				 char **error)
+{
+	enum {
+		pol_name,
+		pol_nrows,
+		pol_seqnn,
+	};
+	struct resmon_jrpc_policy policy[] = {
+		[pol_name] =	 { .key = "name", .type = json_type_string,
+				   .required = true },
+		[pol_nrows] =	 { .key = "nrows", .type = json_type_int,
+				   .required = true },
+		[pol_seqnn] =	 { .key = "seqnn", .type = json_type_int,
+				   .required = true },
+	};
+	struct json_object *values[ARRAY_SIZE(policy)] = {};
+	bool seen[ARRAY_SIZE(policy)] = {};
+	uint32_t nrows;
+	uint32_t seqnn;
+	int err;
+
+	err = resmon_jrpc_dissect(table_obj, policy, seen, values,
+				  ARRAY_SIZE(policy), error);
+	if (err)
+		return err;
+
+	err = resmon_jrpc_extract_uint32(policy[pol_nrows].key,
+					 values[pol_nrows], &nrows, error);
+	if (err != 0)
+		return err;
+
+	err = resmon_jrpc_extract_uint32(policy[pol_seqnn].key,
+					 values[pol_seqnn], &seqnn, error);
+	if (err != 0)
+		return err;
+
+	*ptable = (struct resmon_jrpc_table) {
+		.name = json_object_get_string(values[pol_name]),
+		.nrows = nrows,
+		.seqnn = seqnn,
+	};
+	return 0;
+}
+
+static int
+resmon_jrpc_dissect_get_tables_tables(struct json_object *tables_array,
+				      struct resmon_jrpc_table **ptables,
+				      size_t *pnum_tables, char **error)
+{
+	size_t tables_array_len = json_object_array_length(tables_array);
+	struct resmon_jrpc_table *tables;
+
+	if (resmon_jrpc_validate_array(tables_array, json_type_object,
+				       error) != 0)
+		return -1;
+
+	tables = calloc(tables_array_len, sizeof(*tables));
+	if (tables == NULL) {
+		resmon_fmterr(error, "Couldn't allocate tables: %m");
+		return -1;
+	}
+
+	for (size_t i = 0; i < tables_array_len; i++) {
+		struct json_object *table_obj =
+			json_object_array_get_idx(tables_array, i);
+		struct resmon_jrpc_table *table = &tables[i];
+		int err;
+
+		err = resmon_jrpc_dissect_tables_table(table_obj, table, error);
+		if (err != 0)
+			goto free_tables;
+	}
+
+	*ptables = tables;
+	*pnum_tables = tables_array_len;
+	return 0;
+
+free_tables:
+	free(tables);
+	return -1;
+}
+
+int resmon_jrpc_dissect_get_tables(struct json_object *obj,
+				   struct resmon_jrpc_table **tables,
+				   size_t *num_tables, char **error)
+{
+	/* Result for query with "get_tables" method is supposed to look
+	 * like:
+	 *
+	 * { "tables": [ {"name": "$NAME", "nrows": $NR, "seqnn": $I }, ...] }
+	 */
+	enum {
+		pol_tables,
+	};
+	struct resmon_jrpc_policy policy[] = {
+		[pol_tables] = { .key = "tables", .type = json_type_array,
+				 .required = true },
+	};
+	struct json_object *values[ARRAY_SIZE(policy)] = {};
+	bool seen[ARRAY_SIZE(policy)] = {};
+	int err;
+
+	err = resmon_jrpc_dissect(obj, policy, seen, values,
+				  ARRAY_SIZE(policy), error);
+	if (err != 0)
+		return err;
+
+	return resmon_jrpc_dissect_get_tables_tables(values[pol_tables],
+						     tables, num_tables,
+						     error);
 }
 
 int resmon_jrpc_send(struct resmon_sock *sock, struct json_object *obj)

Debugging/libbpf-tools/resmon/resmon.h
@@ -119,6 +119,15 @@ int resmon_jrpc_dissect_stats(struct json_object *obj,
 			      size_t *num_gauges,
 			      char **error);
 
+struct resmon_jrpc_table {
+	const char *name;
+	uint32_t nrows;
+	uint32_t seqnn;
+};
+int resmon_jrpc_dissect_get_tables(struct json_object *obj,
+				   struct resmon_jrpc_table **tables,
+				   size_t *num_tables, char **error);
+
 int resmon_jrpc_send(struct resmon_sock *sock, struct json_object *obj);
 
 /* resmon-c.c */
@@ -127,6 +136,7 @@ int resmon_c_ping(int argc, char **argv);
 int resmon_c_stop(int argc, char **argv);
 int resmon_c_emad(int argc, char **argv);
 int resmon_c_stats(int argc, char **argv);
+int resmon_c_dump(int argc, char **argv);
 
 /* resmon-stat.c */
 

commit c75cfb3d8bb2e4e724d3922098dd8b21082928cc

resmon: Add 'dump' command to help message
The command appears in the man page, but not in the help message. Add
it.

Fixes: c1879318e5b6 ("resmon: Add list_tables RPC")
Signed-off-by: Ido Schimmel <idosch@nvidia.com>
Reviewed-by: Petr Machata <petrm@nvidia.com>

Debugging/libbpf-tools/resmon/resmon.c
@@ -20,7 +20,7 @@ static int resmon_help(void)
 	     "Usage: resmon [OPTIONS] { COMMAND | help }\n"
 	     "where  OPTIONS := [ -h | --help | -q | --quiet | -v | --verbose |\n"
 	     "			  -V | --version | --sockdir <DIR> | --json ]\n"
-	     "	     COMMAND := { start | stop | ping | emad | stats }\n"
+	     "	     COMMAND := { start | stop | ping | emad | stats | dump }\n"
 	     );
 	return 0;
 }