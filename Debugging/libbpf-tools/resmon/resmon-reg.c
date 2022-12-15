Mellanox/mlxsw

commit b120f6db01fcad94be35bd66b104b65b8b7c1716

resmon: Introduce an EMAD command
The resource monitor needs to parse the individual EMADs to determine which
resource they are pertaining to. There will be two sources of these EMADs:
for real usage, the in-kernel BPF scripts will feed them to resmon through
a ring buffer; for testing, the "mock" backend will allow injecting these
EMADs through the RPC.

In this patch, introduce the RPC-based API. Also introduce a new module,
resmon-reg, where register parsing will eventually be. Currently the module
only parses the overall EMAD structure.

Signed-off-by: Petr Machata <petrm@nvidia.com>
Signed-off-by: Ido Schimmel <idosch@nvidia.com>

Debugging/libbpf-tools/resmon/resmon-reg.c
@@ -1,0 +1,110 @@
+// SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0
+#include <assert.h>
+#include <endian.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "resmon.h"
+
+typedef struct {
+	uint16_t value;
+} uint16_be_t;
+
+static inline uint16_t uint16_be_toh(uint16_be_t be)
+{
+	return be16toh(be.value);
+}
+
+struct resmon_reg_emad_tl {
+	int type;
+	int length;
+};
+
+struct resmon_reg_op_tlv {
+	uint16_be_t type_len;
+	uint8_t status;
+	uint8_t resv2;
+	uint16_be_t reg_id;
+	uint8_t r_method;
+	uint8_t resv3;
+	uint64_t tid;
+};
+
+struct resmon_reg_reg_tlv_head {
+	uint16_be_t type_len;
+	uint16_t reserved;
+};
+
+static struct resmon_reg_emad_tl
+resmon_reg_emad_decode_tl(uint16_be_t type_len_be)
+{
+	uint16_t type_len = uint16_be_toh(type_len_be);
+
+	return (struct resmon_reg_emad_tl){
+		.type = type_len >> 11,
+		.length = type_len & 0x7ff,
+	};
+}
+
+#define RESMON_REG_PULL(size, payload, payload_len)			\
+	({								\
+		if (payload_len < size)					\
+			goto oob;					\
+		__typeof(payload) __ret = payload;			\
+		payload += size;					\
+		payload_len -= size;					\
+		(const void *) __ret;					\
+	})
+
+#define RESMON_REG_READ(size, payload, payload_len)			\
+	({								\
+		__typeof(payload) __payload = payload;			\
+		__typeof(payload_len) __payload_len = payload_len;	\
+		RESMON_REG_PULL(size, __payload, __payload_len);	\
+	})
+
+static void resmon_reg_err_payload_truncated(char **error)
+{
+	resmon_fmterr(error, "EMAD malformed: Payload truncated");
+}
+
+int resmon_reg_process_emad(struct resmon_stat *stat,
+			    const uint8_t *buf, size_t len, char **error)
+{
+	const struct resmon_reg_reg_tlv_head *reg_tlv;
+	const struct resmon_reg_op_tlv *op_tlv;
+	struct resmon_reg_emad_tl tl;
+
+	op_tlv = RESMON_REG_READ(sizeof(*op_tlv), buf, len);
+	tl = resmon_reg_emad_decode_tl(op_tlv->type_len);
+
+	RESMON_REG_PULL(tl.length * 4, buf, len);
+	reg_tlv = RESMON_REG_READ(sizeof(*reg_tlv), buf, len);
+	tl = resmon_reg_emad_decode_tl(reg_tlv->type_len);
+
+	/* Skip over the TLV if it is in fact a STRING TLV. */
+	if (tl.type == MLXSW_EMAD_TLV_TYPE_STRING) {
+		RESMON_REG_PULL(tl.length * 4, buf, len);
+		reg_tlv = RESMON_REG_READ(sizeof(*reg_tlv), buf, len);
+		tl = resmon_reg_emad_decode_tl(reg_tlv->type_len);
+	}
+
+	if (tl.type != MLXSW_EMAD_TLV_TYPE_REG) {
+		resmon_fmterr(error, "EMAD malformed: No register");
+		return -1;
+	}
+
+	/* Get to the register payload. */
+	RESMON_REG_PULL(sizeof(*reg_tlv), buf, len);
+
+	switch (uint16_be_toh(op_tlv->reg_id)) {
+	}
+
+	resmon_fmterr(error, "EMAD malformed: Unknown register");
+	return -1;
+
+oob:
+	resmon_reg_err_payload_truncated(error);
+	return -1;
+}

Debugging/libbpf-tools/resmon/Makefile
@@ -12,6 +12,7 @@ resmon-OBJECTS :=		\
 	$(OUTPUT)/resmon-c.o	\
 	$(OUTPUT)/resmon-d.o	\
 	$(OUTPUT)/resmon-jrpc.o	\
+	$(OUTPUT)/resmon-reg.o	\
 	$(OUTPUT)/resmon-sock.o	\
 	$(OUTPUT)/resmon-stat.o	\
 	#
@@ -46,7 +47,7 @@ $(OUTPUT)/resmon: $(resmon-OBJECTS)
 	$(call msg,BINARY,$@)
 	$(Q)$(CC) $(CFLAGS) -lz $^ -o $@
 
-$(OUTPUT)/%.o: %.c resmon.h $(COMMON_OBJ) | $(OUTPUT)
+$(OUTPUT)/%.o: %.c resmon.h mlxsw.h $(COMMON_OBJ) | $(OUTPUT)
 	$(call msg,CC,$@)
 	$(Q)$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 

Debugging/libbpf-tools/resmon/mlxsw.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0 */
+
+/* EMAD TLV Types */
+enum {
+	MLXSW_EMAD_TLV_TYPE_END,
+	MLXSW_EMAD_TLV_TYPE_OP,
+	MLXSW_EMAD_TLV_TYPE_STRING,
+	MLXSW_EMAD_TLV_TYPE_REG,
+};

Debugging/libbpf-tools/resmon/resmon-back.c
@@ -14,6 +14,12 @@ struct resmon_back_cls {
 
 	int (*get_capacity)(struct resmon_back *back, uint64_t *capacity,
 			    char **error);
+	bool (*handle_method)(struct resmon_back *back,
+			      struct resmon_stat *stat,
+			      const char *method,
+			      struct resmon_sock *peer,
+			      struct json_object *params_obj,
+			      struct json_object *id);
 };
 
 struct resmon_back *resmon_back_init(const struct resmon_back_cls *cls)
@@ -30,6 +36,19 @@ int resmon_back_get_capacity(struct resmon_back *back, uint64_t *capacity,
 			     char **error)
 {
 	return back->cls->get_capacity(back, capacity, error);
+}
+
+bool resmon_back_handle_method(struct resmon_back *back,
+			       struct resmon_stat *stat,
+			       const char *method,
+			       struct resmon_sock *peer,
+			       struct json_object *params_obj,
+			       struct json_object *id)
+{
+	if (back->cls->handle_method == NULL)
+		return false;
+	return back->cls->handle_method(back, stat, method, peer,
+					params_obj, id);
 }
 
 struct resmon_back_mock {
@@ -64,8 +83,110 @@ static int resmon_back_mock_get_capacity(struct resmon_back *back,
 	return 0;
 }
 
+static int resmon_back_mock_emad_decode_payload(uint8_t *dec, const char *enc,
+						size_t dec_len)
+{
+	for (size_t i = 0; i < dec_len; i++) {
+		char buf[3] = {enc[2 * i], enc[2 * i + 1], '\0'};
+		char *endptr = NULL;
+		long byte;
+
+		errno = 0;
+		byte = strtol(buf, &endptr, 16);
+		if (errno || *endptr != '\0')
+			return -1;
+		dec[i] = byte;
+	}
+	return 0;
+}
+
+static void resmon_back_mock_handle_emad(struct resmon_stat *stat,
+					 struct resmon_sock *peer,
+					 struct json_object *params_obj,
+					 struct json_object *id)
+{
+	struct json_object *obj;
+	size_t dec_payload_len;
+	uint8_t *dec_payload;
+	const char *payload;
+	size_t payload_len;
+	char *error;
+	int rc;
+
+	rc = resmon_jrpc_dissect_params_emad(params_obj, &payload,
+					     &payload_len, &error);
+	if (rc != 0) {
+		resmon_d_respond_invalid_params(peer, id, error);
+		free(error);
+		return;
+	}
+
+	if (payload_len % 2 != 0) {
+		resmon_d_respond_invalid_params(peer, id,
+				    "EMAD payload has an odd length");
+		return;
+	}
+
+	dec_payload_len = payload_len / 2;
+	dec_payload = malloc(dec_payload_len);
+	if (dec_payload == NULL)
+		goto err_respond_memerr;
+
+	rc = resmon_back_mock_emad_decode_payload(dec_payload, payload,
+						  dec_payload_len);
+	if (rc != 0) {
+		resmon_d_respond_invalid_params(peer, id,
+				    "EMAD payload expected in hexdump format");
+		goto out;
+	}
+
+
+	rc = resmon_reg_process_emad(stat, dec_payload, dec_payload_len, &error);
+	if (rc != 0) {
+		resmon_d_respond_error(peer, id, resmon_jrpc_e_reg_process_emad,
+				       "EMAD processing error", error);
+		free(error);
+		goto out;
+	}
+
+	obj = resmon_jrpc_new_object(id);
+	if (obj == NULL)
+		return;
+	if (json_object_object_add(obj, "result", NULL))
+		goto err_free_dec_payload;
+
+	resmon_jrpc_send(peer, obj);
+	json_object_put(obj);
+
+out:
+	free(dec_payload);
+	return;
+
+err_free_dec_payload:
+	free(dec_payload);
+	json_object_put(obj);
+err_respond_memerr:
+	resmon_d_respond_memerr(peer, id);
+}
+
+static bool resmon_back_mock_handle_method(struct resmon_back *back,
+					   struct resmon_stat *stat,
+					   const char *method,
+					   struct resmon_sock *peer,
+					   struct json_object *params_obj,
+					   struct json_object *id)
+{
+	if (strcmp(method, "emad") == 0) {
+		resmon_back_mock_handle_emad(stat, peer, params_obj, id);
+		return true;
+	} else {
+		return false;
+	}
+}
+
 const struct resmon_back_cls resmon_back_cls_mock = {
 	.init = resmon_back_mock_init,
 	.fini = resmon_back_mock_fini,
 	.get_capacity = resmon_back_mock_get_capacity,
+	.handle_method = resmon_back_mock_handle_method,
 };

Debugging/libbpf-tools/resmon/resmon-c.c
@@ -263,6 +263,146 @@ int resmon_c_stop(int argc, char **argv)
 	return resmon_c_stop_jrpc();
 }
 
+static void resmon_c_emad_help(void)
+{
+	fprintf(stderr,
+		"Usage: resmon emad [hex | raw] string PAYLOAD\n"
+		"\n"
+	);
+}
+
+static int resmon_c_emad_jrpc(const char *payload, size_t payload_len)
+{
+	struct json_object *payload_obj;
+	struct json_object *params_obj;
+	struct json_object *response;
+	struct json_object *request;
+	struct json_object *result;
+	const int id = 1;
+	int err;
+
+	request = resmon_jrpc_new_request(id, "emad");
+	if (request == NULL)
+		return -1;
+
+	params_obj = json_object_new_object();
+	if (params_obj == NULL) {
+		err = -ENOMEM;
+		goto put_request;
+	}
+
+	payload_obj = json_object_new_string_len(payload, payload_len);
+	if (payload_obj == NULL) {
+		err = -ENOMEM;
+		goto put_params_obj;
+	}
+
+	if (json_object_object_add(params_obj, "payload", payload_obj)) {
+		err = -ENOMEM;
+		goto put_payload_obj;
+	}
+	payload_obj = NULL;
+
+	if (json_object_object_add(request, "params", params_obj)) {
+		err = -1;
+		goto put_params_obj;
+	}
+	params_obj = NULL;
+
+	response = resmon_c_send_request(request);
+	if (response == NULL) {
+		err = -1;
+		goto put_request;
+	}
+
+	if (!resmon_c_handle_response(response, id, json_type_null, &result)) {
+		err = -1;
+		goto put_response;
+	}
+
+	if (env.verbosity > 0)
+		fprintf(stderr, "resmond took the EMAD\n");
+
+	json_object_put(result);
+put_response:
+	json_object_put(response);
+put_payload_obj:
+	json_object_put(payload_obj);
+put_params_obj:
+	json_object_put(params_obj);
+put_request:
+	json_object_put(request);
+	return err;
+}
+
+int resmon_c_emad(int argc, char **argv)
+{
+	char *payload = NULL;
+	size_t payload_len;
+	enum {
+		mode_hex,
+		mode_raw,
+	} mode = mode_hex;
+	int rc = 0;
+
+	while (argc > 0) {
+		if (strcmp(*argv, "string") == 0) {
+			NEXT_ARG();
+			payload = strdup(*argv);
+			if (payload == NULL) {
+				fprintf(stderr, "Failed to strdup: %m\n");
+				rc = -1;
+				goto out;
+			}
+			payload_len = strlen(payload);
+			NEXT_ARG_FWD();
+			break;
+		} else if (strcmp(*argv, "help") == 0) {
+			resmon_c_emad_help();
+			goto out;
+		} else {
+			fprintf(stderr, "What is \"%s\"?\n", *argv);
+			rc = -1;
+			goto out;
+		}
+		continue;
+
+incomplete_command:
+		fprintf(stderr, "Command line is not complete. Try option \"help\"\n");
+		rc = -1;
+		goto out;
+	}
+
+	if (payload == NULL) {
+		fprintf(stderr, "EMAD payload not given.\n");
+		rc = -1;
+		goto out;
+	}
+
+	if (mode == mode_raw) {
+		char *enc_payload = malloc(payload_len * 2 + 1);
+
+		if (enc_payload == NULL) {
+			fprintf(stderr, "Failed to allocate buffer for decoded payload: %m\n");
+			rc = -1;
+			goto out;
+		}
+
+		for (size_t i = 0; i < payload_len; i++)
+			sprintf(&enc_payload[2 * i], "%02x", payload[i]);
+
+		free(payload);
+		payload = enc_payload;
+		payload_len = payload_len * 2;
+	}
+
+	rc = resmon_c_emad_jrpc(payload, payload_len);
+
+out:
+	free(payload);
+	return rc;
+}
+
 static void resmon_c_stats_help(void)
 {
 	fprintf(stderr,

Debugging/libbpf-tools/resmon/resmon-d.c
@@ -304,6 +304,9 @@ static void resmon_d_handle_method(struct resmon_back *back,
 	} else if (strcmp(method, "stats") == 0) {
 		resmon_d_handle_stats(back, stat, peer, params_obj, id);
 		return;
+	} else if (resmon_back_handle_method(back, stat, method,
+					     peer, params_obj, id)) {
+		return;
 	}
 
 	__resmon_d_respond(peer,

Debugging/libbpf-tools/resmon/resmon-jrpc.c
@@ -410,6 +410,32 @@ int resmon_jrpc_dissect_params_empty(struct json_object *obj,
 	return resmon_jrpc_dissect(obj, NULL, NULL, NULL, 0, error);
 }
 
+int resmon_jrpc_dissect_params_emad(struct json_object *obj,
+				    const char **payload,
+				    size_t *payload_len,
+				    char **error)
+{
+	enum {
+		pol_payload,
+	};
+	struct resmon_jrpc_policy policy[] = {
+		[pol_payload] = { .key = "payload", .type = json_type_string,
+				  .required = true },
+	};
+	struct json_object *values[ARRAY_SIZE(policy)] = {};
+	bool seen[ARRAY_SIZE(policy)] = {};
+	int err;
+
+	err = resmon_jrpc_dissect(obj, policy, seen, values,
+				  ARRAY_SIZE(policy), error);
+	if (err)
+		return err;
+
+	*payload = json_object_get_string(values[pol_payload]);
+	*payload_len = json_object_get_string_len(values[pol_payload]);
+	return 0;
+}
+
 static int
 resmon_jrpc_dissect_stats_gauge(struct json_object *gauge_obj,
 				struct resmon_jrpc_gauge *pgauge,

Debugging/libbpf-tools/resmon/resmon.c
@@ -20,7 +20,7 @@ static int resmon_help(void)
 	     "Usage: resmon [OPTIONS] { COMMAND | help }\n"
 	     "where  OPTIONS := [ -h | --help | -q | --quiet | -v | --verbose |\n"
 	     "			  -V | --version | --sockdir <DIR> ]\n"
-	     "	     COMMAND := { start | stop | ping | stats }\n"
+	     "	     COMMAND := { start | stop | ping | emad | stats }\n"
 	     );
 	return 0;
 }
@@ -38,6 +38,9 @@ static int resmon_cmd(int argc, char **argv)
 	} else if (strcmp(*argv, "ping") == 0) {
 		NEXT_ARG_FWD();
 		return resmon_c_ping(argc, argv);
+	} else if (strcmp(*argv, "emad") == 0) {
+		NEXT_ARG_FWD();
+		return resmon_c_emad(argc, argv);
 	} else if (strcmp(*argv, "stats") == 0) {
 		NEXT_ARG_FWD();
 		return resmon_c_stats(argc, argv);

commit 42600f226877ea0c3af16d047de724a8872adce6

resmon: Skip over TLVs until reaching REG TLV
Currently, the code assumes that after the OPERATION TLV we might have a
STRING TLV and then the REG TLV. However, it is also possible to
encounter a LATENCY TLV before the REG TLV.

Therefore, instead of assuming a specific EMAD structure, simply skip
over all the TLVs after the OPERATION TLV until reaching the REG TLV.

Fixes: b120f6db01fc ("resmon: Introduce an EMAD command")
Signed-off-by: Ido Schimmel <idosch@nvidia.com>
Reviewed-by: Petr Machata <petrm@nvidia.com>

Debugging/libbpf-tools/resmon/resmon-reg.c
@@ -1137,20 +1137,10 @@ int resmon_reg_process_emad(struct resmon_reg *rreg,
 	op_tlv = RESMON_REG_READ(sizeof(*op_tlv), buf, len);
 	tl = resmon_reg_emad_decode_tl(op_tlv->type_len);
 
-	RESMON_REG_PULL(tl.length * 4, buf, len);
-	reg_tlv = RESMON_REG_READ(sizeof(*reg_tlv), buf, len);
-	tl = resmon_reg_emad_decode_tl(reg_tlv->type_len);
-
-	/* Skip over the TLV if it is in fact a STRING TLV. */
-	if (tl.type == MLXSW_EMAD_TLV_TYPE_STRING) {
+	while (tl.type != MLXSW_EMAD_TLV_TYPE_REG) {
 		RESMON_REG_PULL(tl.length * 4, buf, len);
 		reg_tlv = RESMON_REG_READ(sizeof(*reg_tlv), buf, len);
 		tl = resmon_reg_emad_decode_tl(reg_tlv->type_len);
-	}
-
-	if (tl.type != MLXSW_EMAD_TLV_TYPE_REG) {
-		resmon_fmterr(error, "EMAD malformed: No register");
-		return -1;
 	}
 
 	/* Get to the register payload. */